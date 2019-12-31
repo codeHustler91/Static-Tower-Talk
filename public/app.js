3aa684f9aa0561200ed5aae842a4d7ee82621329
foggybrains <dustin.keyes@gmail.com>
2019-12-30 19:49:27 -0500

    styling

--- public/app.js
+++ public/app.js
@@ -1,7 +1,10 @@
+// targets
 const title = document.querySelector('h1')
 
+// event listeners
 title.addEventListener('click', (event) => changeTitle())
 
+// functions
 function changeTitle() {
     title.innerText == 'TOWER TALK' ? title.innerText = 'EASTER EGGG!!!' : title.innerText = 'TOWER TALK'
 }
